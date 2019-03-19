2c7ea3271949bce2266c4b303db9abf39a0b2ba7
jnguyentran.oracle <john.nguyen-tran@oracle.com>
2019-03-19 17:06:24 -0400

    Hello

--- 03_instance_ol7.tf
+++ 03_instance_ol7.tf
@@ -17,7 +17,7 @@ resource "oci_core_instance" "WebAppSvr01" {
   display_name = "WebAppSvr01"
   hostname_label = "WebAppSvr01"
   image = "ocid1.image.oc1.iad.aaaaaaaa2tq67tvbeavcmioghquci6p3pvqwbneq3vfy7fe7m7geiga4cnxa"
-  shape = "VM.Standard1.1"
+  shape = "VM.Standard2.1"
   subnet_id = "${oci_core_subnet.ProdSubnetA-public-subnet1.id}"
   metadata {
     ssh_authorized_keys = "${file(var.ssh_public_key_file_ol7)}"
@@ -37,7 +37,7 @@ resource "oci_core_instance" "WebAppSvr02" {
   display_name = "WebAppSvr02"
   hostname_label = "WebAppSvr02"
   image = "ocid1.image.oc1.iad.aaaaaaaa2tq67tvbeavcmioghquci6p3pvqwbneq3vfy7fe7m7geiga4cnxa"
-  shape = "VM.Standard1.1"
+  shape = "VM.Standard2.1"
   subnet_id = "${oci_core_subnet.ProdSubnetB-public-subnet2.id}"
   metadata {
     ssh_authorized_keys = "${file(var.ssh_public_key_file_ol7)}"
